15c24d889995a63e87bb8cc9566cc28272d2f4eb
KinzaAshfaq <89211081+KinzaAshfaq@users.noreply.github.com>
2021-08-20 21:42:09 -0700

    Add files via upload

---
+++ square root.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+ 
+using namespace std;
+ 
+int main()
+{
+	float squareRoot,num;
+
+	cout<<"Enter any number:";
+	cin>>num;
+
+	squareRoot=sqrt(num);
+
+	cout<<"Square root of "<<num<<" is "<<squareRoot;
+
+	return 0;
+}
